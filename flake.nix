970abf87a5bd718a0f7e6fcaa28e5d59f26d8b4f
SHIRAISHI Tatsukichi <shiraishi_tatsukichi@css-crescent.co.jp>
2025-10-21 13:44:01 +0900

    Refactor: Move dev shell into lib and setup toolchain
    This commit moves the dev shell setup into the lib section of the flake.
    It also prepares for a toolchain input to be passed into the dev shell.
    
    Signed-off-by: SHIRAISHI Tatsukichi <shiraishi_tatsukichi@css-crescent.co.jp>

--- flake.nix
+++ flake.nix
@@ -1,76 +1,25 @@
 {
   description = "rust 開発用 Nix ライブラリー";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-
   outputs =
-    { fenix, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      fenixPkgs = fenix.packages.${system};
+    { nixpkgs, ... }:
+    {
+      lib = {
+        mkDevShell =
+          { system, toolchain }:
+          let
+            pkgs = import nixpkgs { inherit system; };
 
-      toolchain = fenixPkgs.fromToolchainFile {
-        file = ./rust-toolchain.toml;
-        sha256 = "sha256-GCGEXGZeJySLND0KU5TdtTrqFV76TF3UdvAHSUegSsk=";
-      };
+            packages = import ./pkgs/pkgs.nix { inherit toolchain pkgs; };
 
-      rustPlatform = pkgs.makeRustPlatform {
-        cargo = toolchain;
-        rustc = toolchain;
-      };
-
-      mdbook-pandoc = import ./pkgs/mdbook-pandoc.nix {
-        inherit rustPlatform pkgs;
-      };
-
-      rustPkgs = [
-        toolchain
-        pkgs.cargo-leptos
-        pkgs.leptosfmt
-        pkgs.wasm-bindgen-cli
-        pkgs.stylance-cli
-        pkgs.sass
-      ];
-
-      mdbookPkgs = [
-        pkgs.mdbook
-        pkgs.mdbook-mermaid
-        pkgs.mdbook-pdf
-        mdbook-pandoc
-      ];
-
-      nixPkgs = with pkgs; [
-        nixd
-        nixfmt-rfc-style
-        nix-tree
-      ];
-
-      commonTools = with pkgs; [
-        git
-        pkg-config
-        google-cloud-sdk
-        glibcLocales
-        zenith
-        pandoc
-      ];
-
-      LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-
-    in
-    rec {
-      packages = rustPkgs ++ mdbookPkgs ++ nixPkgs ++ commonTools;
-      devShells.${system}.default = pkgs.mkShell {
-        name = "rust-nightly-devshell";
-        inherit packages LOCALE_ARCHIVE;
-        # shellHook = ''
-        #   echo "Welcome to the Nix devShell!"
-        # '';
+            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+          in
+          pkgs.mkShell {
+            name = "rust-nightly-devshell";
+            inherit packages LOCALE_ARCHIVE;
+          };
       };
     };
 }
